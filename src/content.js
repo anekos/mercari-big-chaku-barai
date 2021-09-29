7ea79639460f980c7d52855b358a85f9b7c4ffbb
anekos <anekos@snca.net>
2021-09-29 21:57:01 +0900

    Support new mercari

--- src/content.js
+++ src/content.js
@@ -1,7 +1,7 @@
 
 function main() {
-  Array.from(document.querySelectorAll('.item-shipping-fee')).forEach(it => {
-    if (it.textContent === '送料込み')
+  Array.from(document.querySelectorAll('mer-price[data-testid="price"] + span')).forEach(it => {
+    if (0 <= it.textContent.indexOf('送料込み'))
       return
     it.setAttribute('class', it.getAttribute('class', '') + ' akuma-pig-shogun')
   })

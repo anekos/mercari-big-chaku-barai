be34d788cc09cda12af3348b1d96438f6212e37c
anekos <anekos@snca.net>
2021-11-24 21:33:04 +0900

    Follow up

--- src/content.js
+++ src/content.js
@@ -1,10 +1,22 @@
 
+const AkumaPigShogun = 'akuma-pig-shogun'
+
+
 function main() {
-  Array.from(document.querySelectorAll('mer-price[data-testid="price"] + span')).forEach(it => {
-    if (0 <= it.textContent.indexOf('送料込み'))
-      return
-    it.setAttribute('class', it.getAttribute('class', '') + ' akuma-pig-shogun')
-  })
+  let handle = setInterval(
+    () => {
+      Array.from(document.querySelectorAll('mer-display-row > span[slot="body"][data-testid="配送料の負担"]')).forEach(it => {
+        if (0 <= it.textContent.indexOf('送料込み')) {
+          return
+        }
+        let klass = it.getAttribute('class') || '';
+        if (0 <= klass.indexOf(AkumaPigShogun))
+          return
+        it.setAttribute('class', klass + ' ' + AkumaPigShogun)
+      })
+    },
+    500
+  )
 }
 
 main()
